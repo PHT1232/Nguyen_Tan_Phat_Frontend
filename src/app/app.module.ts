8ba251060e5c62903d47b66055647d0567c9ad9c
PHT1232 <57097105+PHT1232@users.noreply.github.com>
2023-06-12 18:57:37 +0700

    update intercep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientJsonpModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientJsonpModule } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -106,6 +106,7 @@ import { UploadComponent } from './main/upload/upload.component';
 import { RetailComponent } from './main/retail/retail.component';
 import { CreateRetailComponent } from './main/retail/create-retail/create-retail.component';
 import { RetailDetailComponent } from './main/retail/retail-detail/retail-detail.component';
+import { AddCsrfHeaderInterceptorService } from '@shared/service-proxies/service-proxies';
 
 @NgModule({
   declarations: [
@@ -221,7 +222,7 @@ import { RetailDetailComponent } from './main/retail/retail-detail/retail-detail
     DialogModule,
     ImageModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AddCsrfHeaderInterceptorService, multi: true }],
   entryComponents: [
     // tenants
     CreateTenantDialogComponent,
